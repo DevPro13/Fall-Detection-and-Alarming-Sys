6aea9892865e8ab7133fdfc1cf7346cc958b1ae1
devpro13 <076bei013.devraj@pcampus.edu.np>
2024-12-12 04:46:04 +0545

    Code updated: modified in elapsed time setup

--- src/main.py
+++ src/main.py
@@ -14,42 +14,37 @@ def logData(data):
         print(not first_char)
         if not first_char:
             file.write("Epoch_time_s,milliseconds_elapsed,Ax,Ay,Az,Gx,Gy,Gz"+"\n") 
-        data_in_text=",".join(str(data_item)
-                              for data_item in data)+"\n"
+        data_in_text=",".join(str(data_item) for data_item in data)+"\n"
         file.write(data_in_text)
         file.flush() # flush data to the file        
 if __name__=='__main__':
-    for i in range(0):
-        #this loop is created for led indicator so that we can complete initial set until led is blinking.
-        if i%2==0:
-            led.value(1)
-        else:
-            led.value(0)
-        time.sleep(1)
+    for i in range(4):  #Blinks led before it starts reading sensors data
+        led.value(i % 2)
+        time.sleep(0.5)
     led.value(0)
-prev_tick=0
-time_in_ms=0#count time in milliseconds
-prev_epoch=0
-while True:
-    ax=round(imu.accel.x,8)
-    ay=round(imu.accel.y,8)
-    az=round(imu.accel.z,8)
-    gx=round(imu.gyro.x,8)
-    gy=round(imu.gyro.y,8)
-    gz=round(imu.gyro.z,8)
-    current_tick=time.ticks_ms()
-    tick_diff=abs(time.ticks_diff(prev_tick,current_tick))
-    current_time_in_epoch_s=time.time()
-    if prev_tick!=0:
-        time_in_ms+=tick_diff
-        if prev_epoch!=current_time_in_epoch_s:
-            time_in_ms=0
-        else:
-            time_in_ms%=1000# value might exceed 1000 ms
-    data=[current_time_in_epoch_s,time_in_ms,ax,ay,az,gx,gy,gz]
-    #time starts from jan 1, 2021 12:00:00 AM
-    print(data)
-    logData(data)
-    prev_tick=current_tick
-    prev_epoch=current_time_in_epoch_s
-    time.sleep(0.2)
+    prev_tick=0
+    time_in_ms=0#count time in milliseconds
+    prev_epoch=0
+    while True:
+        ax=round(imu.accel.x,8)
+        ay=round(imu.accel.y,8)
+        az=round(imu.accel.z,8)
+        gx=round(imu.gyro.x,8)
+        gy=round(imu.gyro.y,8)
+        gz=round(imu.gyro.z,8)
+        current_time_in_epoch_s=time.time()#current
+        current_tick=time.ticks_ms()#milliseconds elapsed
+        tick_diff=abs(time.ticks_diff(prev_tick,current_tick))
+        if prev_tick!=0:
+            time_in_ms+=tick_diff
+            if prev_epoch!=current_time_in_epoch_s:
+                time_in_ms=0
+            else:
+                time_in_ms%=1000# value might exceed 1000 ms
+        data=[current_time_in_epoch_s,time_in_ms,ax,ay,az,gx,gy,gz]
+        #time starts from jan 1, 2021 12:00:00 AM
+        print(data)
+        logData(data)
+        prev_tick=current_tick
+        prev_epoch=current_time_in_epoch_s
+        time.sleep(0.2)
